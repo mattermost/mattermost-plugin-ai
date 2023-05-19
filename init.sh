d1c939a9c05d3d77737a43e36cb6620b0011b27d
Andrew Zigler <andrewzigler@gmail.com>
2023-05-19 14:21:43 -0700

    🐛 fix(init.sh): remove unused environment variables The MM_SERVICESETTINGS_ENABLEDEVELOPER and MM_DEBUG environment variables are not used in the make deploy command, so they are removed to simplify the script.

--- init.sh
+++ init.sh
@@ -22,6 +22,6 @@ docker exec mattermost mmctl --local channel users add $team_name:$channel_name 
 
 export MM_URL="$(gp url 8065)"
 
-MM_SERVICESETTINGS_ENABLEDEVELOPER=true MM_DEBUG=true MM_SERVICESETTINGS_SITEURL=http://localhost:8065 MM_ADMIN_USERNAME=$user_name MM_ADMIN_PASSWORD=$user_password make deploy
+MM_SERVICESETTINGS_SITEURL=http://localhost:8065 MM_ADMIN_USERNAME=$user_name MM_ADMIN_PASSWORD=$user_password make deploy
 
 echo -e "\n===========================\n\n  YOU CAN NOW LOG IN TO MATTERMOST AT $MM_URL\n\n        username:  $user_name\n        password:  $user_password\n\n  THEN ENABLE THE PLUGIN & CONFIGURE YOUR API KEY: $MM_URL/admin_console/plugins/plugin_summarize"
