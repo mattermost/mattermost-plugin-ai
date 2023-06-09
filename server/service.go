1c46fb1ccbd37a3ff5cfa23b415f9ae222c84431
Christopher Speller <crspeller@gmail.com>
2023-06-09 13:08:14 -0700

    Add footer about summary containing inaccuracies.

--- server/service.go
+++ server/service.go
@@ -292,6 +292,7 @@ func (p *Plugin) handleCallRecordingPost(recordingPost *model.Post) (err error) 
 		"",
 		"\n## Key Discussion Points\n",
 		"",
+		"_Summary generated using AI, and may contain inaccuracies. Do not take this summary as absolute truth._",
 	}
 	if err := p.pluginAPI.Post.UpdatePost(botPost); err != nil {
 		return err
