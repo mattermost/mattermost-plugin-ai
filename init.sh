41e944ede62bf2838d114bd1d38fbace82b06149
Andrew Zigler <andrewzigler@gmail.com>
2023-06-16 09:54:38 -0700

    🐛 fix(init.sh): add MM_SERVICESETTINGS_SITEURL environment variable to deploy command The MM_SERVICESETTINGS_SITEURL environment variable was added to the deploy command to specify the URL of the Mattermost server. This is necessary for the Mattermost AI plugin to be deployed correctly.

--- init.sh
+++ init.sh
@@ -22,6 +22,7 @@ docker exec mattermost mmctl --local channel users add $team_name:$channel_name 
 
 MM_ADMIN_USERNAME=$user_name
 MM_ADMIN_PASSWORD=$user_password
+MM_SERVICESETTINGS_SITEURL=http://localhost:8065
 make deploy /mattermost-ai-plugin
 
 #echo -e "\n===========================\n\n  FIRST, DO THIS COMMAND: MM_SERVICESETTINGS_SITEURL=http://localhost:8065 MM_ADMIN_USERNAME=root MM_ADMIN_PASSWORD=$user_password make deploy"
