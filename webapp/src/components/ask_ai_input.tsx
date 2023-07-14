80167edbb275a9389026194656bca64e0c711cc0
Christopher Speller <crspeller@gmail.com>
2023-07-14 13:19:29 -0700

    Moving to styled components and styling fixes.

--- webapp/src/components/ask_ai_input.tsx
+++ webapp/src/components/ask_ai_input.tsx
@@ -1,36 +1,42 @@
 import React, {useState} from 'react';
+import styled from 'styled-components';
+
 import IconAI from './icon_ai';
+
+const Container = styled.div`
+	display: flex
+	position: relative
+	border: 1px solid #ddd
+	borderRadius: 5px
+	padding: 5px
+	right: 7px
+`;
+
+const Input = styled.input`
+	border: none
+	backgroundColor: transparent
+`;
 
 export default function AskAiInput() {
     const [value, setValue] = useState('');
     return (
-        <div
-            onClick={(e) => e.stopPropagation()}
-            style={{
-                border: '1px solid #ddd',
-                borderRadius: 5,
-                display: 'flex',
-                padding: 5,
-                position: 'relative',
-                right: 7,
-            }}
-        >
+        <Container onClick={(e) => e.stopPropagation()}>
             <span className='icon'><IconAI/></span>
-            <input
+            <Input
                 type='text'
                 placeholder='Ask AI about this thread...'
                 value={value}
                 onChange={(e) => setValue(e.target.value)}
                 onKeyDown={(e) => {
-                    const postId = ((e.target as HTMLElement).closest('.post-menu') as HTMLElement).dataset.postid
+                    const postId = ((e.target as HTMLElement).closest('.post-menu') as HTMLElement).dataset.postid;
                     if (e.key === 'Enter') {
                         e.preventDefault();
-                        console.log('TODO: send message to AI for post', postId, value);
+
+                        //console.log('TODO: send message to AI for post', postId, value);
                     }
                 }}
-                style={{border: 'none', backgroundColor: 'transparent'}}
             />
-        </div>
-    )
+        </Container>
+    );
 }
 
--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -9,6 +9,7 @@ import {LLMBotPost} from './components/llmbot_post';
 import IconAI from './components/icon_ai';
 import IconReactForMe from './components/icon_react_for_me';
 import IconThreadSummarization from './components/icon_thread_summarization';
+
 // TODO: waiting for getting the backend implemented
 // import AskAiInput from './components/ask_ai_input';
 import {doReaction, doSummarize, doTranscribe} from './client';
@@ -18,15 +19,16 @@ const BotUsername = 'ai';
 export default class Plugin {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
     public async initialize(registry: any, store: Store<GlobalState, Action<Record<string, unknown>>>) {
-		registry.registerPostTypeComponent('custom_llmbot', LLMBotPost);
+        registry.registerPostTypeComponent('custom_llmbot', LLMBotPost);
         if (registry.registerPostAction) {
             registry.registerPostAction('AI Actions', IconAI, [
+
                 // TODO: waiting for getting the backend implemented
                 // {text: <AskAiInput/>},
                 {text: <><span className='icon'><IconThreadSummarization/></span>{'Summarize Thread'}</>, action: makeSummarizePost(store)},
                 {text: <><span className='icon'><IconThreadSummarization/></span>{'Summarize Meeting Audio'}</>, action: doTranscribe},
                 {text: <><span className='icon'><IconReactForMe/></span>{'React for me'}</>, action: doReaction},
-			]);
+            ]);
         } else {
             // TODO: waiting for getting the backend implemented
             // registry.registerPostDropdownMenuAction(<AskAiInput/>);
