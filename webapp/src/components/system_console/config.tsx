09b180214a7d5a6a0678b974390ca414e11d636c
Christopher Speller <crspeller@gmail.com>
2024-01-18 14:36:49 -0800

    Introduce source available folder. Move multiple LLMs to enterprise. (#108)
    * Introduce source available folder. Move multiple LLMs to enterprise.
    
    * Warn for no services.

--- webapp/src/components/system_console/config.tsx
+++ webapp/src/components/system_console/config.tsx
@@ -1,12 +1,15 @@
 import React, {useCallback} from 'react';
 import styled from 'styled-components';
 
+import {PlusIcon} from '@mattermost/compass-icons/components';
+
+import {TertiaryButton} from '../assets/buttons';
+
+import {useIsMultiLLMLicensed} from '@/license';
+
 import {ServiceData} from './service';
 import ServiceForm from './service_form';
-
-const AddAIServiceButton = styled.button`
-    margin-bottom: 10px;
-`;
+import EnterpriseChip from './enterprise_chip';
 
 type Value = {
     services: ServiceData[],
@@ -36,15 +39,49 @@ type Props = {
     setSaveNeeded: () => void
 }
 
-const SecurityContainer = styled.div`
-    position: relative;
-    padding: 20px;
+const PanelContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+    padding: 32px;
+	gap: 32px;
     border: 1px solid #ccc;
-    margin-bottom: 20px;
     background: white;
-    .form-group {
-        margin: 20px;
-    }
+`;
+
+const PanelHeader = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 4px;
+`;
+
+const PanelTitle = styled.div`
+	font-size: 16px;
+	font-weight: 600;
+`;
+
+const PanelSubtitle = styled.div`
+	color: rgba(63, 67, 80, 0.72);
+	font-size: 14px;
+	font-weight: 400;
+`;
+
+const PlusAIServiceIcon = styled(PlusIcon)`
+	width: 18px;
+	height: 18px;
+	margin-right: 8px;
+`;
+
+const ConfigContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 20px;
+`;
+
+const EnterpriseChipContainer = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	gap: 8px;
 `;
 
 const defaultConfig = {
@@ -59,9 +96,31 @@ const defaultConfig = {
     onlyUsersOnTeam: '',
 };
 
+type PanelProps = {
+    title: string
+    subtitle: string
+    children: React.ReactNode
+}
+
+const Panel = (props: PanelProps) => {
+    return (
+        <PanelContainer>
+            <PanelHeader>
+                <PanelTitle>{props.title}</PanelTitle>
+                <PanelSubtitle>{props.subtitle}</PanelSubtitle>
+            </PanelHeader>
+            <div>
+                {props.children}
+            </div>
+        </PanelContainer>
+    );
+};
+
 const Config = (props: Props) => {
     const value = props.value || defaultConfig;
     const currentServices = value.services;
+    const multiLLMLicensed = useIsMultiLLMLicensed();
+    const licenceAddDisabled = !multiLLMLicensed && currentServices.length > 0;
 
     const addNewService = useCallback((e: React.MouseEvent) => {
         e.preventDefault();
@@ -99,168 +158,153 @@ const Config = (props: Props) => {
         }
     }, [value, currentServices]);
 
-    return (
-        <div>
-            {currentServices.map((service, idx) => (
-                <ServiceForm
-                    key={idx}
-                    service={service}
-                    onDelete={(deletedService) => {
-                        const updatedServiceIdx = currentServices.indexOf(deletedService);
-                        if (updatedServiceIdx === -1) {
-                            throw new Error('Service not found');
-                        }
-                        let newValue = value;
-                        if (currentServices.length > 1) {
-                            if (value.llmBackend === deletedService.name) {
-                                newValue = {...newValue, llmBackend: value.services[0]?.name || ''};
-                            }
-                            if (value.imageGeneratorBackend === deletedService.name) {
-                                newValue = {...newValue, imageGeneratorBackend: value.services[0]?.name || ''};
-                            }
-                            if (value.transcriptBackend === deletedService.name) {
-                                newValue = {...newValue, transcriptBackend: value.services[0]?.name || ''};
-                            }
-                        } else {
-                            newValue = {...newValue, llmBackend: '', transcriptBackend: '', imageGeneratorBackend: ''};
-                        }
-                        props.onChange(props.id, {...newValue, services: [...currentServices.slice(0, updatedServiceIdx), ...currentServices.slice(updatedServiceIdx + 1)]});
-                        props.setSaveNeeded();
-                    }}
-                    onChange={(changedService) => {
-                        const updatedServiceIdx = currentServices.findIndex((s) => changedService.id === s.id);
-                        if (updatedServiceIdx === -1) {
-                            throw new Error('Service not found');
-                        }
-                        let newValue = value;
-                        if (value.llmBackend === currentServices[updatedServiceIdx].name) {
-                            newValue = {...newValue, llmBackend: changedService.name};
-                        }
-                        if (value.imageGeneratorBackend === currentServices[updatedServiceIdx].name) {
-                            newValue = {...newValue, imageGeneratorBackend: changedService.name};
-                        }
-                        if (value.transcriptBackend === currentServices[updatedServiceIdx].name) {
-                            newValue = {...newValue, transcriptBackend: changedService.name};
-                        }
-                        props.onChange(props.id, {...newValue, services: [...currentServices.slice(0, updatedServiceIdx), changedService, ...currentServices.slice(updatedServiceIdx + 1)]});
-                        props.setSaveNeeded();
-                    }}
-                />
-            ))}
-            <AddAIServiceButton
-                className='save-button btn btn-primary'
-                onClick={addNewService}
-            >
-                {'Add AI Service'}
-            </AddAIServiceButton>
-            <div className='form-group'>
-                <label
-                    className='control-label col-sm-4'
-                    htmlFor='ai-llm-backend'
-                >
-                    {'AI Large Language Model service'}
-                </label>
-                <div className='col-sm-8'>
-                    <select
-                        id='ai-llm-backend'
-                        className={currentServices.length === 0 ? 'form-control disabled' : 'form-control'}
-                        onChange={(e) => {
-                            props.onChange(props.id, {...value, llmBackend: e.target.value});
-                            props.setSaveNeeded();
-                        }}
-                        value={value.llmBackend}
-                        disabled={currentServices.length === 0}
-                    >
-                        {currentServices.map((service) => (
-                            <option
-                                key={service.id}
-                                value={service.name}
-                            >
-                                {service.name}
-                            </option>
-                        ))}
-                    </select>
-                    {currentServices.length === 0 && (
-                        <div className='help-text'>
-                            <span>{'You need at least one AI services use this setting.'}</span>
-                        </div>
-                    )}
-                </div>
-            </div>
-            <div className='form-group'>
-                <label
-                    className='control-label col-sm-4'
-                    htmlFor='ai-image-generator'
-                >
-                    {'AI Image Generator service'}
-                </label>
-                <div className='col-sm-8'>
-                    <select
-                        id='ai-image-generator'
-                        className={currentServices.length === 0 ? 'form-control disabled' : 'form-control'}
-                        onChange={(e) => {
-                            props.onChange(props.id, {...value, imageGeneratorBackend: e.target.value});
-                            props.setSaveNeeded();
-                        }}
-                        value={value.imageGeneratorBackend}
-                        disabled={currentServices.length === 0}
-                    >
-                        {currentServices.map((service) => (
-                            <option
-                                key={service.id}
-                                value={service.name}
-                            >
-                                {service.name}
-                            </option>
-                        ))}
-                    </select>
-                    {currentServices.length === 0 && (
-                        <div className='help-text'>
-                            <span>{'You need at least one AI services use this setting.'}</span>
-                        </div>
-                    )}
-                </div>
-            </div>
-            <div className='form-group'>
-                <label
-                    className='control-label col-sm-4'
-                    htmlFor='ai-transcript-backend'
-                >
-                    {'AI Audio/Video transcript service'}
-                </label>
-                <div className='col-sm-8'>
-                    <select
-                        id='ai-transcript-backend'
-                        className={currentServices.length === 0 ? 'form-control disabled' : 'form-control'}
-                        onChange={(e) => {
-                            props.onChange(props.id, {...value, transcriptBackend: e.target.value});
-                            props.setSaveNeeded();
-                        }}
-                        value={value.transcriptBackend}
-                        disabled={currentServices.length === 0}
-                    >
-                        {currentServices.map((service) => (
-                            <option
-                                key={service.id}
-                                value={service.name}
-                            >
-                                {service.name}
-                            </option>
-                        ))}
-                    </select>
-                    {currentServices.length === 0 && (
-                        <div className='help-text'>
-                            <span>{'You need at least one AI services use this setting.'}</span>
-                        </div>
-                    )}
-                </div>
-            </div>
+    const deleteService = (deletedService: ServiceData) => {
+        const updatedServiceIdx = currentServices.indexOf(deletedService);
+        if (updatedServiceIdx === -1) {
+            throw new Error('Service not found');
+        }
+        let newValue = value;
+        if (currentServices.length > 1) {
+            if (value.llmBackend === deletedService.name) {
+                newValue = {...newValue, llmBackend: value.services[0]?.name || ''};
+            }
+            if (value.imageGeneratorBackend === deletedService.name) {
+                newValue = {...newValue, imageGeneratorBackend: value.services[0]?.name || ''};
+            }
+            if (value.transcriptBackend === deletedService.name) {
+                newValue = {...newValue, transcriptBackend: value.services[0]?.name || ''};
+            }
+        } else {
+            newValue = {...newValue, llmBackend: '', transcriptBackend: '', imageGeneratorBackend: ''};
+        }
+        props.onChange(props.id, {...newValue, services: [...currentServices.slice(0, updatedServiceIdx), ...currentServices.slice(updatedServiceIdx + 1)]});
+        props.setSaveNeeded();
+    };
 
-            <SecurityContainer className='AdminPanel'>
-                <div className='header'>
-                    <h3><span>{'User restrictions'}</span></h3>
-                    <div className='mt-2'><span>{'Enable restrictions to allow or not users to use AI in this instance.'}</span></div>
+    const changeService = (changedService: ServiceData) => {
+        const updatedServiceIdx = currentServices.findIndex((s) => changedService.id === s.id);
+        if (updatedServiceIdx === -1) {
+            throw new Error('Service not found');
+        }
+        let newValue = value;
+        if (value.llmBackend === currentServices[updatedServiceIdx].name) {
+            newValue = {...newValue, llmBackend: changedService.name};
+        }
+        if (value.imageGeneratorBackend === currentServices[updatedServiceIdx].name) {
+            newValue = {...newValue, imageGeneratorBackend: changedService.name};
+        }
+        if (value.transcriptBackend === currentServices[updatedServiceIdx].name) {
+            newValue = {...newValue, transcriptBackend: changedService.name};
+        }
+        props.onChange(props.id, {...newValue, services: [...currentServices.slice(0, updatedServiceIdx), changedService, ...currentServices.slice(updatedServiceIdx + 1)]});
+        props.setSaveNeeded();
+    };
+
+    return (
+        <ConfigContainer>
+            <Panel
+                title='AI Services'
+                subtitle='Multiple AI services can be configured with the AI plugin.'
+            >
+                {currentServices.map((service) => (
+                    <ServiceForm
+                        key={service.id}
+                        service={service}
+                        onDelete={deleteService}
+                        onChange={changeService}
+                    />
+                ))}
+                <EnterpriseChipContainer>
+                    <TertiaryButton
+                        onClick={addNewService}
+                        disabled={licenceAddDisabled}
+                    >
+                        <PlusAIServiceIcon/>
+                        {'Add AI Service'}
+                    </TertiaryButton>
+                    {licenceAddDisabled && (
+                        <EnterpriseChip subtext={'Multiple AI services is available on Enterprise plans'}/>
+                    )}
+                </EnterpriseChipContainer>
+            </Panel>
+            <Panel
+                title='AI functions'
+                subtitle='If you have more than one AI server you can choose which service to use for each function.'
+            >
+                <div className='form-group'>
+                    <label
+                        className='control-label col-sm-4'
+                        htmlFor='ai-llm-backend'
+                    >
+                        {'AI Large Language Model service'}
+                    </label>
+                    <div className='col-sm-8'>
+                        <select
+                            id='ai-llm-backend'
+                            className={currentServices.length === 0 ? 'form-control disabled' : 'form-control'}
+                            onChange={(e) => {
+                                props.onChange(props.id, {...value, llmBackend: e.target.value});
+                                props.setSaveNeeded();
+                            }}
+                            value={value.llmBackend}
+                            disabled={currentServices.length === 0}
+                        >
+                            {currentServices.map((service) => (
+                                <option
+                                    key={service.id}
+                                    value={service.name}
+                                >
+                                    {service.name}
+                                </option>
+                            ))}
+                        </select>
+                        {currentServices.length === 0 && (
+                            <div className='help-text'>
+                                <span>{'You need at least one AI services use this setting.'}</span>
+                            </div>
+                        )}
+                    </div>
                 </div>
+                <div className='form-group'>
+                    <label
+                        className='control-label col-sm-4'
+                        htmlFor='ai-transcript-backend'
+                    >
+                        {'AI Audio/Video transcript service'}
+                    </label>
+                    <div className='col-sm-8'>
+                        <select
+                            id='ai-transcript-backend'
+                            className={currentServices.length === 0 ? 'form-control disabled' : 'form-control'}
+                            onChange={(e) => {
+                                props.onChange(props.id, {...value, transcriptBackend: e.target.value});
+                                props.setSaveNeeded();
+                            }}
+                            value={value.transcriptBackend}
+                            disabled={currentServices.length === 0}
+                        >
+                            {currentServices.map((service) => (
+                                <option
+                                    key={service.id}
+                                    value={service.name}
+                                >
+                                    {service.name}
+                                </option>
+                            ))}
+                        </select>
+                        {currentServices.length === 0 && (
+                            <div className='help-text'>
+                                <span>{'You need at least one AI services use this setting.'}</span>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </Panel>
+
+            <Panel
+                title='User restrictions (experimental)'
+                subtitle='Enable restrictions to allow or not users to use AI in this instance.'
+            >
                 <div className='form-group'>
                     <label
                         className='control-label col-sm-4'
@@ -354,7 +398,7 @@ const Config = (props: Props) => {
                         </div>
                     </>
                 )}
-            </SecurityContainer>
+            </Panel>
 
             <div className='form-group'>
                 <label
@@ -385,7 +429,7 @@ const Config = (props: Props) => {
                     <div className='help-text'><span>{'Enable tracing of LLM requests. Outputs whole conversations to the logs.'}</span></div>
                 </div>
             </div>
-        </div>
+        </ConfigContainer>
     );
 };
 export default Config;
---
+++ webapp/src/license.tsx
@@ -0,0 +1,83 @@
+import {useSelector} from 'react-redux';
+
+import {GlobalState} from '@mattermost/types/store';
+
+const e10 = 'E10';
+const e20 = 'E20';
+const professional = 'professional';
+const enterprise = 'enterprise';
+
+// isValidSkuShortName returns whether the SKU short name is one of the known strings;
+// namely: E10 or professional, or E20 or enterprise
+const isValidSkuShortName = (license: Record<string, string>) => {
+    switch (license?.SkuShortName) {
+    case e10:
+    case e20:
+    case professional:
+    case enterprise:
+        return true;
+    default:
+        return false;
+    }
+};
+
+const checkE20Licensed = (license: Record<string, string>) => {
+    if (license?.SkuShortName === e20 || license?.SkuShortName === enterprise) {
+        return true;
+    }
+
+    if (!isValidSkuShortName(license)) {
+        // As a fallback for licenses whose SKU short name is unknown, make a best effort to try
+        // and use the presence of a known E20/Enterprise feature as a check to determine licensing.
+        if (license?.MessageExport === 'true') {
+            return true;
+        }
+    }
+
+    return false;
+};
+
+const checkE10Licensed = (license: Record<string, string>) => {
+    if (license?.SkuShortName === e10 || license?.SkuShortName === professional ||
+        license?.SkuShortName === e20 || license?.SkuShortName === enterprise) {
+        return true;
+    }
+
+    if (!isValidSkuShortName(license)) {
+        // As a fallback for licenses whose SKU short name is unknown, make a best effort to try
+        // and use the presence of a known E10/Professional feature as a check to determine licensing.
+        if (license?.LDAP === 'true') {
+            return true;
+        }
+    }
+
+    return false;
+};
+
+const isConfiguredForDevelopment = (state: GlobalState): boolean => {
+    const config = state.entities.general.config;
+
+    return config.EnableTesting === 'true' && config.EnableDeveloper === 'true';
+};
+
+// isE20LicensedOrDevelopment returns true when the server is licensed with a legacy Mattermost
+// Enterprise E20 License or a Mattermost Enterprise License, or has `EnableDeveloper` and
+// `EnableTesting` configuration settings enabled, signaling a non-production, developer mode.
+export const isE20LicensedOrDevelopment = (state: GlobalState): boolean => {
+    const license = state.entities.general.license;
+
+    return checkE20Licensed(license) || isConfiguredForDevelopment(state);
+};
+
+// isE10LicensedOrDevelopment returns true when the server is at least licensed with a legacy Mattermost
+// Enterprise E10 License or a Mattermost Professional License, or has `EnableDeveloper` and
+// `EnableTesting` configuration settings enabled, signaling a non-production, developer mode.
+const isE10LicensedOrDevelopment = (state: GlobalState): boolean => {
+    const license = state.entities.general.license;
+
+    return checkE10Licensed(license) || isConfiguredForDevelopment(state);
+};
+
+export function useIsMultiLLMLicensed() {
+    return useSelector(isE10LicensedOrDevelopment);
+}
