7a33a358a1876783c9a2e9fb557e8ffc0bb4339f
Christopher Speller <crspeller@gmail.com>
2024-04-25 15:16:34 -0700

    Add more fields to JIRA issue fetching

--- server/built_in_tools.go
+++ server/built_in_tools.go
@@ -197,39 +197,121 @@ type GetJiraIssueArgs struct {
 var validJiraIssueKey = regexp.MustCompile(`^([[:alnum:]]+)-([[:digit:]]+)$`)
 
 func formatJiraIssue(issue *jira.Issue) string {
-	key := issue.Key
-	summary := ""
-	description := ""
-	status := ""
-	assignee := ""
-	created := ""
-	comments := ""
+	result := strings.Builder{}
+	result.WriteString("Issue Key: ")
+	result.WriteString(issue.Key)
+	result.WriteRune('\n')
+
 	if issue.Fields != nil {
-		summary = issue.Fields.Summary
-		description = issue.Fields.Description
+		result.WriteString("Summary: ")
+		result.WriteString(issue.Fields.Summary)
+		result.WriteRune('\n')
 
+		result.WriteString("Description: ")
+		result.WriteString(issue.Fields.Description)
+		result.WriteRune('\n')
+
+		result.WriteString("Status: ")
 		if issue.Fields.Status != nil {
-			status = issue.Fields.Status.Name
+			result.WriteString(issue.Fields.Status.Name)
 		} else {
-			status = "Unknown"
+			result.WriteString("Unknown")
 		}
+		result.WriteRune('\n')
 
+		result.WriteString("Assignee: ")
 		if issue.Fields.Assignee != nil {
-			assignee = issue.Fields.Assignee.DisplayName
+			result.WriteString(issue.Fields.Assignee.DisplayName)
 		} else {
-			assignee = "Unassigned"
+			result.WriteString("Unassigned")
+		}
+		result.WriteRune('\n')
+
+		result.WriteString("Created: ")
+		result.WriteString(time.Time(issue.Fields.Created).Format(time.RFC1123))
+		result.WriteRune('\n')
+
+		result.WriteString("Updated: ")
+		result.WriteString(time.Time(issue.Fields.Updated).Format(time.RFC1123))
+		result.WriteRune('\n')
+
+		if issue.Fields.Type.Name != "" {
+			result.WriteString("Type: ")
+			result.WriteString(issue.Fields.Type.Name)
+			result.WriteRune('\n')
 		}
 
-		created = time.Time(issue.Fields.Created).Format(time.RFC1123)
+		if issue.Fields.Labels != nil {
+			result.WriteString("Labels: ")
+			result.WriteString(strings.Join(issue.Fields.Labels, ", "))
+			result.WriteRune('\n')
+		}
+
+		if issue.Fields.Reporter != nil {
+			result.WriteString("Reporter: ")
+			result.WriteString(issue.Fields.Reporter.DisplayName)
+			result.WriteRune('\n')
+		} else if issue.Fields.Creator != nil {
+			result.WriteString("Creator: ")
+			result.WriteString(issue.Fields.Creator.DisplayName)
+			result.WriteRune('\n')
+		}
+
+		if issue.Fields.Priority != nil {
+			result.WriteString("Priority: ")
+			result.WriteString(issue.Fields.Priority.Name)
+			result.WriteRune('\n')
+		}
+
+		if !time.Time(issue.Fields.Duedate).IsZero() {
+			result.WriteString("Due Date: ")
+			result.WriteString(time.Time(issue.Fields.Duedate).Format(time.RFC1123))
+			result.WriteRune('\n')
+		}
+
+		if issue.Fields.TimeTracking != nil {
+			if issue.Fields.TimeTracking.OriginalEstimate != "" {
+				result.WriteString("Original Estimate: ")
+				result.WriteString(issue.Fields.TimeTracking.OriginalEstimate)
+				result.WriteRune('\n')
+			}
+			if issue.Fields.TimeTracking.TimeSpent != "" {
+				result.WriteString("Time Spent: ")
+				result.WriteString(issue.Fields.TimeTracking.TimeSpent)
+				result.WriteRune('\n')
+			}
+			if issue.Fields.TimeTracking.RemainingEstimate != "" {
+				result.WriteString("Remaining Estimate: ")
+				result.WriteString(issue.Fields.TimeTracking.RemainingEstimate)
+				result.WriteRune('\n')
+			}
+		}
 
 		if issue.Fields.Comments != nil {
 			for _, comment := range issue.Fields.Comments.Comments {
-				comments += fmt.Sprintf("Comment from %s at %s: %s\n", comment.Author.DisplayName, comment.Created, comment.Body)
+				result.WriteString(fmt.Sprintf("Comment from %s at %s: %s\n", comment.Author.DisplayName, comment.Created, comment.Body))
 			}
 		}
 	}
 
-	return fmt.Sprintf("Issue Key: %s\nSummary: %s\nDescription: %s\nStatus: %s\nAssignee: %s\nCreated: %s\nComments:\n%s\n", key, summary, description, status, assignee, created, comments)
+	return result.String()
+}
+
+var fetchedFields = []string{
+	"summary",
+	"description",
+	"status",
+	"assignee",
+	"created",
+	"updated",
+	"issuetype",
+	"labels",
+	"reporter",
+	"creator",
+	"priority",
+	"duedate",
+	"timetracking",
+	"comment",
 }
 
 func getPublicJiraIssues(instanceURL string, issueKeys []string) ([]jira.Issue, error) {
@@ -238,7 +320,7 @@ func getPublicJiraIssues(instanceURL string, issueKeys []string) ([]jira.Issue, 
 		return nil, fmt.Errorf("failed to create Jira client: %w", err)
 	}
 	jql := fmt.Sprintf("key in (%s)", strings.Join(issueKeys, ","))
-	issues, _, err := client.Issue.Search(jql, &jira.SearchOptions{Fields: []string{"summary", "description", "status", "assignee", "created", "comment"}})
+	issues, _, err := client.Issue.Search(jql, &jira.SearchOptions{Fields: fetchedFields})
 	if err != nil {
 		return nil, fmt.Errorf("failed to get issue: %w", err)
 	}
