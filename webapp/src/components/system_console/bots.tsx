e2191f64406c30ee6a8380d43896600e87eae77c
Christopher Speller <crspeller@gmail.com>
2024-05-28 15:31:41 -0700

    Rename from 'AI Copilot' to 'Copilot'

--- webapp/src/components/system_console/bots.tsx
+++ webapp/src/components/system_console/bots.tsx
@@ -32,7 +32,7 @@ const defaultNewBot = {
 export const firstNewBot = {
     ...defaultNewBot,
     name: 'ai',
-    displayName: 'AI Copilot',
+    displayName: 'Copilot',
 };
 
 type Props = {
@@ -49,7 +49,7 @@ const Bots = (props: Props) => {
         e.preventDefault();
         const id = Math.random().toString(36).substring(2, 22);
         if (props.bots.length === 0) {
-            // Suggest the '@ai' and 'AI Copilot' name for the first bot
+            // Suggest the '@ai' and 'Copilot' name for the first bot
             props.onChange([{
                 ...firstNewBot,
                 id,
