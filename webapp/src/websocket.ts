600e4a3501fba8caf16061073ab6f9b02359f013
gunhee <xgunheex@gmail.com>
2025-01-11 01:06:53 +0900

    * Changed the type of `WebsocketListeners` from `Map` to `WebsocketListenerObject[]`. (#269)

--- webapp/src/websocket.ts
+++ webapp/src/websocket.ts
@@ -3,21 +3,34 @@ import {WebSocketMessage} from '@mattermost/client';
 import {PostUpdateWebsocketMessage} from './components/llmbot_post';
 
 type WebsocketListener = (msg: WebSocketMessage<PostUpdateWebsocketMessage>) => void
-type WebsocketListeners = Map<string, WebsocketListener>
+type WebsocketListenerObject = {
+    postID: string;
+    listenerID: string;
+    listener: WebsocketListener;
+}
+type WebsocketListeners = WebsocketListenerObject[]
 
 export default class PostEventListener {
-    postUpdateWebsocketListeners: WebsocketListeners = new Map<string, WebsocketListener>();
+    postUpdateWebsocketListeners: WebsocketListeners = [];
 
-    public registerPostUpdateListener = (postID: string, listener: WebsocketListener) => {
-        this.postUpdateWebsocketListeners.set(postID, listener);
+    public registerPostUpdateListener = (postID: string, listenerID: string, listener: WebsocketListener) => {
+        this.postUpdateWebsocketListeners.push({postID, listenerID, listener});
     };
 
-    public unregisterPostUpdateListener = (postID: string) => {
-        this.postUpdateWebsocketListeners.delete(postID);
+    public unregisterPostUpdateListener = (postID: string, listenerID: string) => {
+        this.postUpdateWebsocketListeners = this.postUpdateWebsocketListeners.filter((listenerObject) => {
+            const isSamePostID = listenerObject.postID === postID;
+            const isSameListenerID = listenerObject.listenerID === listenerID;
+            return !(isSamePostID && isSameListenerID);
+        });
     };
 
     public handlePostUpdateWebsockets = (msg: WebSocketMessage<PostUpdateWebsocketMessage>) => {
         const postID = msg.data.post_id;
-        this.postUpdateWebsocketListeners.get(postID)?.(msg);
+        this.postUpdateWebsocketListeners.forEach((listenerObject) => {
+            if (listenerObject.postID === postID) {
+                listenerObject.listener(msg);
+            }
+        });
     };
 }
