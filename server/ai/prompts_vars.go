c624b770dd8ed7166ebc706fdfa0b3ad45d6f3c0
Christopher Speller <crspeller@gmail.com>
2025-01-16 16:42:53 -0800

    Some cleanup in the prompting system.  (#279)
    * Add find "action items" and "open questions" to the post menu dropdown.
    
    * Some prompt system refactoring.

---
+++ server/ai/prompts_vars.go
@@ -0,0 +1,19 @@
+package ai
+
+// Automatically generated convenience vars for the filenames in ai/prompts/
+const (
+	PromptDirectMessageQuestion            = "direct_message_question"
+	PromptEmojiSelect                      = "emoji_select"
+	PromptFindActionItems                  = "find_action_items"
+	PromptFindActionItemsSince             = "find_action_items_since"
+	PromptFindOpenQuestions                = "find_open_questions"
+	PromptFindOpenQuestionsSince           = "find_open_questions_since"
+	PromptLocale                           = "locale"
+	PromptMeetingSummary                   = "meeting_summary"
+	PromptMeetingSummaryGeneral            = "meeting_summary_general"
+	PromptStandardPersonality              = "standard_personality"
+	PromptStandardPersonalityWithoutLocale = "standard_personality_without_locale"
+	PromptSummarizeChannelSince            = "summarize_channel_since"
+	PromptSummarizeChunk                   = "summarize_chunk"
+	PromptSummarizeThread                  = "summarize_thread"
+)
